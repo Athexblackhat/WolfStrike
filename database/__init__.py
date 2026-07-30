650c01794aaf0cb6354646ac021b57f2108a18ae
Athex black hat <athexhacker@gmail.com>
2026-07-30 15:59:13 +0500

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -16,9 +16,12 @@ from database.models import (
     ModuleResult,
     ScanError,
     SessionData,
+    Migration,
+    Base,
     init_database,
     get_session,
     close_session,
+    DatabaseOperations,
 )
 
 __all__ = [
@@ -27,11 +30,68 @@ __all__ = [
     'ModuleResult',
     'ScanError',
     'SessionData',
+    'Migration',
+    'Base',
     'init_database',
     'get_session',
     'close_session',
+    'DatabaseOperations',
+    'create_all_tables',
+    'drop_all_tables',
 ]
 
 __version__ = '1.0.0'
 __author__ = 'ATHEX BLACK HAT'
 __team__ = 'Wolf Intelligence PK'
+
+
+def create_all_tables(db_path: str = 'database/wolfstrike.db', echo: bool = False) -> None:
+    """
+    Create all database tables.
+    
+    Args:
+        db_path: Path to SQLite database file
+        echo: Enable SQL query logging
+    """
+    from sqlalchemy import create_engine
+    from sqlalchemy.pool import StaticPool
+    
+    engine = create_engine(
+        f'sqlite:///{db_path}',
+        echo=echo,
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    
+    Base.metadata.create_all(engine)
+    engine.dispose()
+
+
+def drop_all_tables(db_path: str = 'database/wolfstrike.db', echo: bool = False) -> None:
+    """
+    Drop all database tables (WARNING: Destructive).
+    
+    Args:
+        db_path: Path to SQLite database file
+        echo: Enable SQL query logging
+    """
+    import warnings
+    
+    warnings.warn(
+        "drop_all_tables() will delete ALL data in the database!",
+        UserWarning,
+        stacklevel=2
+    )
+    
+    from sqlalchemy import create_engine
+    from sqlalchemy.pool import StaticPool
+    
+    engine = create_engine(
+        f'sqlite:///{db_path}',
+        echo=echo,
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    
+    Base.metadata.drop_all(engine)
+    engine.dispose()
